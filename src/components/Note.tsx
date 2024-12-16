ddebcfb7b09bc5157882c6a04c37fd4c7e9e7443
Travis Peters <Travis.Peters@ivanti.com>
2024-12-16 08:31:16 -0700

    more stuff

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,75 +1,47 @@
-// import * as Dialog from "@radix-ui/react-dialog";
-// import { Cross2Icon } from "@radix-ui/react-icons";
-
-// const DialogDemo = () => (
-//   <Dialog.Root>
-//     <Dialog.Trigger asChild>
-//       <button className="Button violet">Edit profile</button>
-//     </Dialog.Trigger>
-//     <Dialog.Portal>
-//       <Dialog.Overlay className="DialogOverlay" />
-//       <Dialog.Content className="DialogContent">
-//         <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-//         <Dialog.Description className="DialogDescription">
-//           Make changes to your profile here. Click save when you're done.
-//         </Dialog.Description>
-//         <fieldset className="Fieldset">
-//           <label className="Label" htmlFor="name">
-//             Name
-//           </label>
-//           <input className="Input" id="name" defaultValue="Pedro Duarte" />
-//         </fieldset>
-//         <fieldset className="Fieldset">
-//           <label className="Label" htmlFor="username">
-//             Username
-//           </label>
-//           <input className="Input" id="username" defaultValue="@peduarte" />
-//         </fieldset>
-//         <div
-//           style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
-//         >
-//           <Dialog.Close asChild>
-//             <button className="Button green">Save changes</button>
-//           </Dialog.Close>
-//         </div>
-//         <Dialog.Close asChild>
-//           <button className="IconButton" aria-label="Close">
-//             <Cross2Icon />
-//           </button>
-//         </Dialog.Close>
-//       </Dialog.Content>
-//     </Dialog.Portal>
-//   </Dialog.Root>
-// );
-
-// export default DialogDemo;
-
+import { Button } from "@/components/ui/button";
 import {
-  ICellRendererComp,
-  ICellRendererParams,
-} from "@ag-grid-community/core";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-export class CompanyLogoRenderer implements ICellRendererComp {
-  eGui!: HTMLSpanElement;
-
-  // Optional: Params for rendering. The same params that are passed to the cellRenderer function.
-  init(params: ICellRendererParams) {
-    let companyLogo: HTMLImageElement = document.createElement("img");
-    companyLogo.src = `https://www.ag-grid.com/example-assets/software-company-logos/${params.value.toLowerCase()}.svg`;
-    companyLogo.setAttribute("class", "logo");
-
-    this.eGui = document.createElement("span");
-    this.eGui.setAttribute("class", "imgSpanLogo");
-    this.eGui.appendChild(companyLogo);
-  }
-
-  // Required: Return the DOM element of the component, this is what the grid puts into the cell
-  getGui() {
-    return this.eGui;
-  }
-
-  // Required: Get the cell to refresh.
-  refresh(params: ICellRendererParams): boolean {
-    return false;
-  }
+export default function Note() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- tailwind.config_old.js
+++ tailwind.config_old.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
@@ -49,6 +49,16 @@ module.exports = {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		}
   	}
